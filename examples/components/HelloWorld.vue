4a24aefccadb18993594ce3012ab8f2abb7728bc
unknown <cosmos.liang@YATSENGLOBAL.COM>
2021-07-06 11:45:47 +0800

    '初始化'

---
+++ examples/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <colorPicker v-model="color" v-on:change="headleChangeColor"></colorPicker>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data () {
+		return {
+			color: '#ff0000'
+		}
+	},
+	methods: {
+		headleChangeColor () {
+			console.log('颜色改变')
+		}
+	}
+}
+</script>
+
